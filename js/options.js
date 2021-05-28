ea64e42e0934e2567071aca9577452fcc1b88974
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-28 17:11:20 -0400

    Added functionality to change notification time based on user input.

--- js/options.js
+++ js/options.js
@@ -1,19 +1,18 @@
 const nameInput = document.getElementById("name-input");
+const timeInput = document.getElementById("time-input");
 const saveButton = document.getElementById("save-btn");
 
 saveButton.addEventListener("click", () => {
   const name = nameInput.value;
+  const notificationTime = timeInput.value;
 
-  chrome.storage.sync.set(
-    {
-      name,
-    },
-    () => {
-      console.log(`Name has been set to ${name}`);
-    }
-  );
-
-  chrome.storage.sync.get(["name"], (res) => {
-    nameInput.value = res.name ?? "User";
+  chrome.storage.sync.set({
+    name,
+    notificationTime,
   });
+});
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
+  nameInput.value = res.name ?? "User";
+  timeInput.value = res.notificationTime ?? 1000;
 });
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,32 @@
 chrome.alarms.create({
-  periodInMinutes: 1 / 60,
+    periodInMinutes: 1 / 60,
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log(alarm);
-  chrome.storage.local.get(["timer"], (res) => {
-    const time = res.timer ?? 0;
-    chrome.storage.local.set({
-      timer: time + 1,
+
+    chrome.storage.local.get(["timer"], (res) => {
+
+        const time = res.timer ?? 0;
+
+        chrome.storage.local.set({
+            timer: time + 1,
+        });
+
+        chrome.action.setBadgeText({
+            text: `${time + 1}`,
+        });
+
+        chrome.storage.sync.get(["notificationTime"], (res) => {
+
+            const notificationTime = res.notificationTime ?? 1000;
+
+            if (time % notificationTime === 0) {
+                this.registration.showNotification("Chrome Timer Extension", {
+                    body: `${notificationTime} seconds have passed..`,
+                    icon: "assets/clock.png",
+                });
+            }
+
+        });
     });
-    chrome.action.setBadgeText({
-      text: `${time + 1}`,
-    });
-    if (time % 1000 === 0) {
-      this.registration.showNotification("Chrome Timer Extension", {
-        body: "1000 seconds have passed..",
-        icon: "assets/clock.png",
-      });
-    }
-  });
 });
