fa51ac5f1bcf37300a8718cf7f83eaba64d7eb58
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-28 14:44:09 -0400

    Configured the chrome's storage API & stored/fetched user name from it.

--- options.js
+++ options.js
@@ -2,5 +2,18 @@ const nameInput = document.getElementById("name-input");
 const saveButton = document.getElementById("save-btn");
 
 saveButton.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+
+  chrome.storage.sync.set(
+    {
+      name,
+    },
+    () => {
+      console.log(`Name has been set to ${name}`);
+    }
+  );
+
+  chrome.storage.sync.get(["name"], (res) => {
+    nameInput.value = res.name ?? "User";
+  });
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
 const timeElement = document.getElementById('time');
 const currentTime = new Date().toLocaleTimeString();
+const nameElement = document.getElementById('name');
 
 timeElement.textContent = `The time is : ${currentTime}`;
 
-chrome.action.setBadge({
+chrome.action.setBadgeText({
     text: 'TIME'
 }, () => {
     console.log('Finished setting badge text.');
+});
+
+chrome.storage.sync.get(['name'], (res) => {
+    const name = res.name ?? "User";
+    nameElement.textContent = `Hello, ${name}`;
 });
